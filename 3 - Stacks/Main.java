8946fcb65a2c4b152611bcc957bd0d34355f2b4e
Natalio Fernandes <116610989+NatalioF22@users.noreply.github.com>
2023-02-22 13:42:50 -0500

    Add files via upload

---
+++ 3 - Stacks/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        Stacks stack = new Stacks();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.pop();
+        stack.printStack();
+
+    }
+}
